1dbee5b2e597fad76260c0d84acb6e4a1612e0ce
Raheem Kawojue <kawojue08@gmail.com>
2023-08-21 03:18:56 +0100

    format:lvl msg

---
+++ db/levels/message.level.ts
@@ -0,0 +1,18 @@
+export const messageLevels = [
+    { id: 1, name: 'Novice Enthusiast', pointRange: [0, 20] },
+    { id: 2, name: 'Luminary Artist', pointRange: [20, 40] },
+    { id: 3, name: 'Masterful Messenger', pointRange: [40, 60] },
+    { id: 4, name: 'Mystical Messenger', pointRange: [60, 80] },
+    { id: 5, name: 'Eloquent Emissary', pointRange: [80, 100] },
+    { id: 6, name: 'Cheerful Chatterbox', pointRange: [100, 120] },
+    { id: 7, name: 'Captivating Communicator', pointRange: [120, 140] },
+    { id: 8, name: 'Starry Performer', pointRange: [140, 160] },
+    { id: 9, name: 'Virtuoso Voyager', pointRange: [160, 180] },
+    { id: 10, name: 'Quirky Quester', pointRange: [180, 200] },
+    { id: 11, name: 'Wordsmith Wanderer', pointRange: [200, 220] },
+    { id: 12, name: 'Apprentice Entertainer', pointRange: [220, 240] },
+    { id: 13, name: 'Jovial Jester', pointRange: [240, 260] },
+    { id: 14, name: 'Artistic Artisan', pointRange: [260, 280] },
+    { id: 15, name: 'Merry Message Maven', pointRange: [280, 300] },
+    { id: 16, name: 'Whimsical Wordsmith', pointRange: [300, 320] }
+]
