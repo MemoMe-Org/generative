8e800b1126899c4befd32db0913cb815d3fa9980
Raheem Kawojue <kawojue08@gmail.com>
2023-08-21 03:58:45 +0100

    [set] level routes

--- routes/root.route.ts
+++ routes/root.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { question } from '../controllers/quesion.controller'
+import { levels } from '../controllers/level.controller'
+import { question } from '../controllers/question.controller'
 
 const root: Router = Router()
 
+root.get('/levels/:levelType', levels)
 root.get('/questions/:questionType', question)
 
 export default root
