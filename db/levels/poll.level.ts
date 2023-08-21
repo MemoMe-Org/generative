273e5da1c06eb98b81d3451538870c878f654031
Raheem Kawojue <kawojue08@gmail.com>
2023-08-21 03:19:35 +0100

    format:poll lvl

---
+++ db/levels/poll.level.ts
@@ -0,0 +1,18 @@
+export const pollLevels = [
+    { id: 1, name: 'Anonymous Aficionado', pointRange: [0, 20] },
+    { id: 2, name: 'Enigmatic Engager', pointRange: [20, 40] },
+    { id: 3, name: 'Polling Paragon', pointRange: [40, 60] },
+    { id: 4, name: 'Whimsical Whisperer', pointRange: [60, 80] },
+    { id: 5, name: 'Fun-loving Luminary', pointRange: [80, 100] },
+    { id: 6, name: 'Playful Pollster', pointRange: [100, 120] },
+    { id: 7, name: 'Polling Prestige', pointRange: [120, 140] },
+    { id: 8, name: 'Option Oracle', pointRange: [140, 160] },
+    { id: 9, name: 'Curious Compiler', pointRange: [160, 180] },
+    { id: 10, name: 'Enthusiastic Examiner', pointRange: [180, 200] },
+    { id: 11, name: 'Questionnaire Curator', pointRange: [200, 220] },
+    { id: 12, name: 'Playful Pollster', pointRange: [220, 240] },
+    { id: 13, name: 'Survey Sage', pointRange: [240, 260] },
+    { id: 14, name: 'Polling Prodigy', pointRange: [260, 280] },
+    { id: 15, name: 'Polling Pioneer', pointRange: [280, 300] },
+    { id: 16, name: 'Polling Picasso', pointRange: [300, 320] }
+]
