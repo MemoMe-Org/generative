e56fd6410ccbc1ea4ecd2d9df3593b42c0c5bd56
Raheem Kawojue <kawojue08@gmail.com>
2023-08-21 03:18:37 +0100

    ..

---
+++ db/levels/index.ts
@@ -0,0 +1,11 @@
+import { pollLevels } from './poll.level'
+import { messageLevels } from './message.level'
+import { overallLevels } from './overall.level'
+
+const levels = {
+    poll: pollLevels,
+    message: messageLevels,
+    overall: overallLevels,
+}
+
+export default levels
