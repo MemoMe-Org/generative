b29cf80572e4419942328de36eff9aa85717d119
Raheem Kawojue <kawojue08@gmail.com>
2023-08-26 22:00:57 +0100

    rm:overall poll

--- db/levels/index.ts
+++ db/levels/index.ts
@@ -1,11 +1,9 @@
 import { pollLevels } from './poll.level'
 import { messageLevels } from './message.level'
-import { overallLevels } from './overall.level'
 
 const levels = {
     poll: pollLevels,
     message: messageLevels,
-    overall: overallLevels,
 }
 
 export default levels
