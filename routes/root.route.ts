26395fa40243a22524948d621a93dbaee2f0b8f4
Raheem Kawojue <kawojue08@gmail.com>
2023-09-18 12:49:05 +0100

    [add] bio route

--- routes/root.route.ts
+++ routes/root.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { levels } from '../controllers/level.controller'
 import { question } from '../controllers/question.controller'
+import { bios } from '../controllers/bio.controller'
 
 const root: Router = Router()
 
+root.get('/bios/', bios)
 root.get('/levels/:levelType', levels)
 root.get('/questions/:questionType', question)
 
