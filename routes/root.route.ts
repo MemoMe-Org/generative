89846442199c7ae24fd7953c17eb393dfc741676
Raheem Kawojue <kawojue08@gmail.com>
2023-09-27 00:34:37 +0100

    deploment stuff

--- routes/root.route.ts
+++ routes/root.route.ts
@@ -1,11 +1,16 @@
-import { Router } from 'express'
+import { Router, Response, Request } from 'express'
+import { bios } from '../controllers/bio.controller'
 import { levels } from '../controllers/level.controller'
 import { question } from '../controllers/question.controller'
-import { bios } from '../controllers/bio.controller'
 
 const root: Router = Router()
 
-root.get('/bios/', bios)
+root.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'MemoMe Generators'
+    })
+})
+root.get('/bios', bios)
 root.get('/levels/:levelType', levels)
 root.get('/questions/:questionType', question)
 
